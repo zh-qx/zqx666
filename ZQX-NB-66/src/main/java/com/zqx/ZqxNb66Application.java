59d7e0a573a0c99b0d7a5de8e30594ffab956464
zh-qx <zhouqix@139.com>
2019-08-16 20:35:27 +0800

    zqx修改成功

--- ZQX-NB-66/src/main/java/com/zqx/ZqxNb66Application.java
+++ ZQX-NB-66/src/main/java/com/zqx/ZqxNb66Application.java
@@ -2,10 +2,10 @@ package com.zqx;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//zqx的springboot工具
 @SpringBootApplication
 public class ZqxNb66Application {
-	//
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ZqxNb66Application.class, args);
 	}
