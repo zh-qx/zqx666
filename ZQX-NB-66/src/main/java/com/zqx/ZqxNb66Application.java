ffcac3d50d36f03f94e9f3da822444af98176df9
zqx <zqx@DESKTOP-UTL6RHD>
2019-08-16 20:14:24 +0800

    zqx的springboot

---
+++ ZQX-NB-66/src/main/java/com/zqx/ZqxNb66Application.java
@@ -0,0 +1,13 @@
+package com.zqx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZqxNb66Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZqxNb66Application.class, args);
+	}
+
+}
